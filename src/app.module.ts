baa12c7705dd8b3d4d259712448bfa6b12c38ffc
jesus alberto garcia hernandez <jesusalberto278@gmail.com>
2023-10-09 09:48:15 -0400

    delete controller/service main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DataServicesModule } from './services/data-services/data-services.module';
 
 @Module({
     imports: [ProductsModule, UsersModule, DataServicesModule],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
